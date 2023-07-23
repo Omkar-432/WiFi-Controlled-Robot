8357e48b6c4c370ba7fbab4936326b0efbb6cd83
Omkar Jagdish Mondkar <128500205+Omkar-432@users.noreply.github.com>
2023-07-23 22:32:35 +0530

    Update Wifi_Robot.ino

--- WiFi Controlled Robot/Wifi_Robot.ino
+++ WiFi Controlled Robot/Wifi_Robot.ino
@@ -1,7 +1,4 @@
-/* https://arduino.esp8266.com/stable/package_esp8266com_index.json
- * ESP8266 as Web Server using WiFi Access Point (AP) mode
- * Connect to AP "Robot Wifi", password = "87654321"
- */
+
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h> 
